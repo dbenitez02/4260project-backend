21d2573fe20df408b5cad01696bdb0511fbce9ff
dbenitez02 <dbenitez101@gmail.com>
2022-12-05 23:39:32 -0800

    backend

---
+++ src/main/java/cpp/edu/backend/entity/Course.java
@@ -0,0 +1,8 @@
+package cpp.edu.backend.entity;
+
+import lombok.Data;
+import javax.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.math.BigDecimal;
+import java.util.Date;
