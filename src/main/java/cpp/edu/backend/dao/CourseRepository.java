9276c4f2519203f37c222e2b2c801b069a9e87d5
dbenitez02 <36527173+D-Benz@users.noreply.github.com>
2022-12-07 14:51:47 -0800

    changes to CourseRepository

--- src/main/java/cpp/edu/backend/dao/CourseRepository.java
+++ src/main/java/cpp/edu/backend/dao/CourseRepository.java
@@ -1,10 +1,14 @@
 package cpp.edu.backend.dao;
 
 import cpp.edu.backend.entity.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200")
-        public interface CourseRepository extends JpaRepository<Course,Long>{
-
+public interface CourseRepository extends JpaRepository<Course,Long>{
+        Page<Course> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
+        Page<Course> findByDepartmentId(@RequestParam("id") Long id, Pageable pageable);
 }
